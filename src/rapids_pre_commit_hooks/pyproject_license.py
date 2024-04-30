c2c91145f0d3e3d00ee2b3ff60f56e227ed4fd6b
Kyle Edwards <kyedwards@nvidia.com>
2024-04-30 13:17:53 -0400

    Add verify-pyproject-license hook

---
+++ src/rapids_pre_commit_hooks/pyproject_license.py
@@ -0,0 +1,71 @@
+# Copyright (c) 2024, NVIDIA CORPORATION.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import copy
+import random
+
+import tomlkit
+import tomlkit.exceptions
+
+from .lint import LintMain
+
+RAPIDS_LICENSE = "Apache 2.0"
+
+
+def find_value_location(document, key):
+    copied_document = copy.deepcopy(document)
+    node = copied_document
+    while str(placeholder := random.randint(0, 1048576)) in node.as_string():
+        pass
+
+    while len(key) > 1:
+        node = node[key[0]]
+        key = key[1:]
+    node[key[0]] = placeholder
+
+    begin_loc = copied_document.as_string().find(str(placeholder))
+    end_loc = (
+        begin_loc
+        + len(str(placeholder))
+        - len(copied_document.as_string())
+        + len(document.as_string())
+    )
+    return begin_loc, end_loc
+
+
+def check_pyproject_license(linter, args):
+    document = tomlkit.loads(linter.content)
+    try:
+        license_value = document["project"]["license"]
+    except tomlkit.exceptions.NonExistentKey:
+        return
+
+    if license_value != RAPIDS_LICENSE:
+        loc = find_value_location(document, ("project", "license"))
+        linter.add_warning(
+            loc, f'license should be "{RAPIDS_LICENSE}"'
+        ).add_replacement(loc, tomlkit.string(RAPIDS_LICENSE).as_string())
+
+
+def main():
+    m = LintMain()
+    m.argparser.description = (
+        f'Verify that pyproject.toml has the correct license ("{RAPIDS_LICENSE}").'
+    )
+    with m.execute() as ctx:
+        ctx.add_check(check_pyproject_license)
+
+
+if __name__ == "__main__":
+    main()
---
+++ test/rapids_pre_commit_hooks/test_pyproject_license.py
@@ -0,0 +1,109 @@
+# Copyright (c) 2024, NVIDIA CORPORATION.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from textwrap import dedent
+
+import pytest
+import tomlkit
+
+from rapids_pre_commit_hooks import pyproject_license
+from rapids_pre_commit_hooks.lint import Linter
+
+
+@pytest.mark.parametrize(
+    ["key", "loc"],
+    [
+        (
+            ("table", "key1"),
+            (15, 22),
+        ),
+        (
+            ("table", "key2"),
+            (30, 32),
+        ),
+        (
+            ("table", "key3"),
+            (40, 60),
+        ),
+        (
+            ("table", "key3", "nested"),
+            (51, 58),
+        ),
+    ],
+)
+def test_find_value_location(key, loc):
+    CONTENT = dedent(
+        """\
+        [table]
+        key1 = "value"
+        key2 = 42
+        key3 = { nested = "value" }
+        """
+    )
+    parsed_doc = tomlkit.loads(CONTENT)
+    assert pyproject_license.find_value_location(parsed_doc, key) == loc
+
+
+@pytest.mark.parametrize(
+    ["document", "loc"],
+    [
+        (
+            dedent(
+                """\
+                [project]
+                license = "Apache-2.0"
+                """
+            ),
+            (20, 32),
+        ),
+        (
+            dedent(
+                """\
+                [project]
+                license = "BSD"
+                """
+            ),
+            (20, 25),
+        ),
+        (
+            dedent(
+                """\
+                [project]
+                license = "Apache 2.0"
+                """
+            ),
+            None,
+        ),
+        (
+            dedent(
+                """\
+                [project]
+                license = 'Apache 2.0'  # Single quotes are fine
+                """
+            ),
+            None,
+        ),
+        ("", None),
+    ],
+)
+def test_check_pyproject_license(document, loc):
+    linter = Linter("pyproject.toml", document)
+    pyproject_license.check_pyproject_license(linter, None)
+
+    expected_linter = Linter("pyproject.toml", document)
+    if loc:
+        expected_linter.add_warning(
+            loc, 'license should be "Apache 2.0"'
+        ).add_replacement(loc, '"Apache 2.0"')
+    assert linter.warnings == expected_linter.warnings
