8b5c22391959d07806bbcea2274cbe13d403b3ed
Kyle Edwards <kyedwards@nvidia.com>
2024-05-06 10:15:03 -0400

    Use UUID instead of random integer

--- src/rapids_pre_commit_hooks/pyproject_license.py
+++ src/rapids_pre_commit_hooks/pyproject_license.py
@@ -13,7 +13,7 @@
 # limitations under the License.
 
 import copy
-import random
+import uuid
 
 import tomlkit
 import tomlkit.exceptions
@@ -29,19 +29,18 @@ ACCEPTABLE_LICENSES = {
 
 def find_value_location(document, key):
     copied_document = copy.deepcopy(document)
-    i = 0
-    while str(placeholder := random.randint(0, 1048576)) in copied_document.as_string():
-        if i := i + 1 >= 10000:
-            raise RuntimeError("Could not find suitable placeholder")
+    placeholder = uuid.uuid4()
 
     node = copied_document
     while len(key) > 1:
         node = node[key[0]]
         key = key[1:]
     old_value = node[key[0]]
-    node[key[0]] = placeholder
+    node[key[0]] = str(placeholder)
 
-    begin_loc = copied_document.as_string().find(str(placeholder))
+    begin_loc = copied_document.as_string().find(
+        tomlkit.string(str(placeholder)).as_string()
+    )
     end_loc = begin_loc + len(old_value.as_string())
     return begin_loc, end_loc
 
@@ -74,17 +73,14 @@ def check_pyproject_license(linter, args):
                 f"{{ text = {tomlkit.string(RAPIDS_LICENSE).as_string()} }}\n",
             )
         else:
-            i = 0
-            while (
-                str(placeholder := random.randint(0, 1048576)) in document.as_string()
-            ):
-                if i := i + 1 >= 10000:
-                    raise RuntimeError("Could not find suitable placeholder")
+            placeholder = uuid.uuid4()
             copied_document = copy.deepcopy(document)
             copied_document["project"].add(
-                str(placeholder), tomlkit.integer(placeholder)
+                str(placeholder), tomlkit.string(str(placeholder))
             )
-            index = copied_document.as_string().find(f"{placeholder} = {placeholder}")
+            index = copied_document.as_string().find(
+                f"{placeholder} = {tomlkit.string(str(placeholder)).as_string()}"
+            )
 
             loc = (index, index)
             linter.add_warning(
