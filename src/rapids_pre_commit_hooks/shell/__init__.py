ffaa5c8be46962d20731e4032300f134fb00e3c0
Kyle Edwards <kyedwards@nvidia.com>
2024-08-23 14:24:16 -0400

    Review feedback

--- src/rapids_pre_commit_hooks/shell/__init__.py
+++ src/rapids_pre_commit_hooks/shell/__init__.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import bashlex  # type: ignore
+import bashlex
 
 from ..lint import ExecutionContext, LintMain
 
--- test/rapids_pre_commit_hooks/test_shell.py
+++ test/rapids_pre_commit_hooks/test_shell.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import bashlex  # type: ignore
+import bashlex
 
 from rapids_pre_commit_hooks.lint import Linter
 from rapids_pre_commit_hooks.shell.verify_conda_yes import VerifyCondaYesVisitor
