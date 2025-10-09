a2a7a3c12ce745130ddb3209c23b1193f55cef8f
Kyle Edwards <kyedwards@nvidia.com>
2025-10-09 15:24:10 -0400

    Oops

--- src/rapids_pre_commit_hooks/copyright.py
+++ src/rapids_pre_commit_hooks/copyright.py
@@ -75,6 +75,7 @@ LONG_FORM_LICENSE_TEXT: dict[str, list[list[str]]] = {
             "the License.",
         ],
         [
+            "",
             'Licensed under the Apache License, Version 2.0 (the "License");',
             "you may not use this file except in compliance with the License.",
             "You may obtain a copy of the License at",
