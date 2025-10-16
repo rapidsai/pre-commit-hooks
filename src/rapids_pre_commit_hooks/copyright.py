08f902b9823dd30c8face808276c689a90ea068e
Kyle Edwards <kyedwards@nvidia.com>
2025-10-16 13:39:33 -0400

    Add an Apache-2.0 with a typo

--- src/rapids_pre_commit_hooks/copyright.py
+++ src/rapids_pre_commit_hooks/copyright.py
@@ -102,6 +102,21 @@ LONG_FORM_LICENSE_TEXT: dict[str, list[list[str]]] = {
             "See the License for the specific language governing permissions and",  # noqa: E501
             "limitations under the License.",
         ],
+        [
+            "",
+            'Licensed under the Apache License, Version 2.0 (the "License");',
+            "you may not use this file except in compliance with the License.",
+            "You may obtain a copy of the License at",
+            "",
+            # This one is a weird typo, but I've seen it a few times
+            "    h ttp://www.apache.org/licenses/LICENSE-2.0",
+            "",
+            "Unless required by applicable law or agreed to in writing, software",  # noqa: E501
+            'distributed under the License is distributed on an "AS IS" BASIS,',  # noqa: E501
+            "WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.",  # noqa: E501
+            "See the License for the specific language governing permissions and",  # noqa: E501
+            "limitations under the License.",
+        ],
     ],
 }
 
